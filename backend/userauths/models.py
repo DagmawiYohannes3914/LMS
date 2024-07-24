2e28f306c3255f8b5690ad4e17901f183b780c3d
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-07-24 11:04:04 +0300

    adding auth model

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100)
+    otp = models.CharField(unique=True, max_length=100)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
