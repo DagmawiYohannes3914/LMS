fb47b15ecfa26095c2082fecb852f6bc62ece1b5
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-07-31 15:32:25 +0300

    registration serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework_simplejwt.serializers import TokenObtainSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from userauths.models import Profile, User
 
 
-class MyTokenObtainSerializer(TokenObtainSerializer):
+class MyTokenObtainSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
@@ -13,6 +14,21 @@ class MyTokenObtainSerializer(TokenObtainSerializer):
         token['username'] = user.username
 
         return token
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['full_name', 'email', 'password', 'password2']
+
+    def validate(self, attr):
+        if attr['password'] != attr['password2']:
+            raise serializers.ValidationError(
+                {"password": "password fields didn't match"})
 
 
 class UserSerializer(serializers.ModelSerializer):
