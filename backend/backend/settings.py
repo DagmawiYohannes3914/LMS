cb89ea58c60f9f6078f08be1cd3fdea9accf3dd5
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-07-31 17:28:43 +0300

    updating settings and urls

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from datetime import timedelta
 import os
+from datetime import timedelta
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
